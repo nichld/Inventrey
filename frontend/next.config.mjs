0a5b6f407697c9450828003d98135e2fbb7b6056
Nichlas <141312928+nichld@users.noreply.github.com>
2024-10-25 06:27:41 +0200

    fixed

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://10.12.7.7:5000",
-    },
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
